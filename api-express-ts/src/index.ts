d7bf5f00d2816b4b721197f0c5d3eadb39404d90
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-15 08:37:20 -0300

    chore: change log started application

--- api-express-ts/src/index.ts
+++ api-express-ts/src/index.ts
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT || 3333, () => console.log("Server running"));
+app.listen(process.env.PORT || 3333, () => console.log("Server running 🐳"));
