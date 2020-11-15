8ced9da28421456d288a4447d111ff92cdca25bc
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-15 08:15:55 -0300

    chore: create sub directory to app ts express called dir api-expressts

--- README.md
+++
@@ -1,1 +0,0 @@
-![example workflow name](https://github.com/actions/hello-world/workflows/Greet%20Everyone/badge.svg)
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { router } from "./resources/user/create_user/endpoint";
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
-
-app.listen(process.env.PORT || 3333, () => console.log("Server running"));
