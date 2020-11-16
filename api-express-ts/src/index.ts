ac2266820e6463a8a7001fc1d0cd984c3020f636
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-16 14:25:46 -0300

    add module web

--- api-express-ts/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { routes } from "./resources/user/create_user/endpoint";
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-
-app.listen(process.env.PORT || 3333, () => console.log("Server running 🐳"));
