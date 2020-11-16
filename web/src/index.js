0136065cb651e5f0d61862d1e2b82f468b6b872a
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-16 14:35:07 -0300

    change msg

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-
+const msg = "Server Running"
 app.get("/", (req, res) =>
   res.json({
-    msg: "OK",
+    msg
   })
 );
 
-app.listen(process.env.PORT || 3333, () => console.log("Server Running"));
+app.listen(process.env.PORT || 3333, () => console.log(msg));
