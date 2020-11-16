a2a3f4b3252d4260329ef8e9ad24c316dbfe50ed
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-16 15:53:47 -0300

    feat: add static files

--- web/src/server.js
+++ web/src/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const msg = "Running !";
+const path = require("path")
+app.use('/', express.static(path.resolve(__dirname,"..","public")));
 
-app.use(express.static("public"));
-
-app.get("/api", (req, res) =>
-  res.json({
-    msg,
-  })
-);
+// app.get("/api", (req, res) =>
+//   res.json({
+//     msg,
+//   })
+// );
 
 app.listen(process.env.PORT || 3333, () => console.log(msg));
