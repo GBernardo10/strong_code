086285d88a35152a878b59138ab7c488793bad30
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-19 19:34:40 -0300

    aula adsb

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-const { getDado } = require("./service/get-data");
+
+// const path = require("path");
+// const { getDado } = require("./service/get-data");
 
 const app = express();
-const msg = "Running !";
+const msg = "API RODANDO !";
 
 app.use(cors());
-app.use("/", express.static(path.resolve(__dirname, "..", "public")));
+// app.use("/", express.static(path.resolve(__dirname, "..", "public")));
 
 app.get("/api", (_, res) =>
   res.json({
-    data: getDado(),
+    data: msg,
   })
 );
 
