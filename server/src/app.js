9a1e2291500a9cfad403877c422f35ada554289f
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-19 15:18:55 -0300

    add cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
 const msg = "Running !";
 
+app.use(cors())
 app.use("/", express.static(path.resolve(__dirname, "..", "public")));
 
 app.get("/api", (_, res) =>
