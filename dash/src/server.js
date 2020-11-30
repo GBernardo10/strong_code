a510284a00c86d1aded845dbc364206d706f1995
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-30 18:02:00 -0300

    deploy

---
+++ dash/src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const msg = "SERVIDOR RODANDO COM SUCESSO!";
+
+const path = require("path")
+
+app.use('/', express.static(path.resolve(__dirname,"..","public")));
+
+app.get("/api", (req, res) =>
+  res.json({
+    msg,
+  })
+);
+
+app.listen(process.env.PORT || 3333, () => console.log(msg));
