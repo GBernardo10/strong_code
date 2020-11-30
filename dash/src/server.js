9a6a6ca28b242ab05c407d112c65db6df63304c1
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-30 18:24:21 -0300

    deploy 25

--- dash/src/server.js
+++ dash/src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const msg = "SERVIDOR RODANDO COM SUCESSOOOOOO!";
+const msg = "SERVIDOR SUCESSOOOOOO!";
 
 const path = require("path")
 
